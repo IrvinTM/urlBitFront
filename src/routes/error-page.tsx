4f0cd3a4007b6dbb65c8b4647e7e4080415c6ac5
IrvinTM <irvint698@gmail.com>
2024-08-29 17:04:13 -0600

    error type

--- src/routes/error-page.tsx
+++ src/routes/error-page.tsx
@@ -1,15 +1,25 @@
-import { useRouteError } from "react-router-dom";
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
+
+  let errorMessage: string;
+
+  if (isRouteErrorResponse(error)) {
+    errorMessage = error.statusText || error.data;
+  } else if (error instanceof Error) {
+    errorMessage = error.message;
+  } else {
+    errorMessage = "An unknown error occurred.";
+  }
 
   return (
     <div id="error-page" className="flex justify-center items-center pt-10">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
-        <i>{error.statusText || error.message}</i>
+        <i>{errorMessage}</i>
       </p>
     </div>
   );
